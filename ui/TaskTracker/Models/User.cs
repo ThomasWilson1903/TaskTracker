e67c990177a6b5e71da1f1a47131fcbc70a7d4b2
Bogachew MV <mmaxbossee@gmail.com>
2025-10-14 19:07:02 +0400

    Добавил пользователю на создание генерацию Guid

--- ui/TaskTracker/Models/User.cs
+++ ui/TaskTracker/Models/User.cs
@@ -1,6 +1,13 @@
-﻿namespace TaskTracker.Models;
+﻿using System;
+
+namespace TaskTracker.Models;
 
 public class User
 {
-    
+    public Guid Id { get; set; }
+
+    public User()
+    {
+        Id = Guid.NewGuid();
+    }
 }
