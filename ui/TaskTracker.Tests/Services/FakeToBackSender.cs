1463d48279ec518004a28f5f6eb1b7aed9f0e590
Bogachew MV <mmaxbossee@gmail.com>
2025-10-22 19:01:54 +0400

    Сделал фейковый сервис отправки данных для тестов

---
+++ ui/TaskTracker.Tests/Services/FakeToBackSender.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using TaskTracker.Models;
+using TaskTracker.Services;
+
+namespace TaskTracker.Tests.Services;
+
+public class FakeToBackSender : IToBackSender
+{
+    public Collection<User> Users = new Collection<User>();
+
+    public Task SendData<T>(string path, T data)
+    {
+        if (data is null)
+            throw new ArgumentNullException(nameof(data));
+
+        if (data is User user)
+            Users.Add(user);
+
+        return Task.CompletedTask;
+    }
+
+    public async Task<IEnumerable<T>?> GetDatas<T>(string path) =>
+        await Task.FromResult((List<T>)(typeof(T) == typeof(User) ? (object)Users.ToList() : new List<T>())).ConfigureAwait(false);
+}
