196d9020ae9d754522ab2a09e73d82d34030253f
Bogachew MV <mmaxbossee@gmail.com>
2025-10-22 19:01:40 +0400

    Написал интерфейс сервиса отправки данных

---
+++ ui/TaskTracker/Services/IToBackSender.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Services;
+
+public interface IToBackSender
+{
+    public Task SendData<T>(string path, T data);
+    
+    public Task<IEnumerable<T>?> GetDatas<T>(string path);
+}
