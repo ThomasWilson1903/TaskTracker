1df0bdbfa1675b313e73125d8725d4264591d213
Bogachew MV <mmaxbossee@gmail.com>
2025-10-22 19:02:14 +0400

    Написал логику пользовательского репозитория

--- ui/TaskTracker/Repositories/UserRepository.cs
+++ ui/TaskTracker/Repositories/UserRepository.cs
@@ -1,6 +1,29 @@
-﻿namespace TaskTracker.Repositories;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskTracker.Models;
+using TaskTracker.Services;
+
+namespace TaskTracker.Repositories;
 
 public class UserRepository
 {
+    private readonly IToBackSender _context;
     
+    public ICollection<User> Users { get; } = new List<User>();
+
+    public UserRepository(IToBackSender context)
+    {
+        _context = context;
+    }
+
+    public async Task Create(User user)
+    {
+        Users.Add(user);
+        await _context.SendData("/user", user).ConfigureAwait(false);
+    }
+
+    public void Delete(User user)
+    {
+        Users.Remove(user);
+    }
 }
